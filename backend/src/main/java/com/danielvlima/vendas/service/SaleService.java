11eace23a7ec39d160c9d02173b01bddc984fe84
Daniel Lima <danielvlima98@gmail.com>
2021-11-05 12:44:02 -0300

    group by search

--- backend/src/main/java/com/danielvlima/vendas/service/SaleService.java
+++ backend/src/main/java/com/danielvlima/vendas/service/SaleService.java
@@ -1,6 +1,9 @@
 package com.danielvlima.vendas.service;
 
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.danielvlima.vendas.dto.SaleDTO;
+import com.danielvlima.vendas.dto.SaleSuccessDTO;
+import com.danielvlima.vendas.dto.SaleSumDTO;
 import com.danielvlima.vendas.entities.Sale;
 import com.danielvlima.vendas.repositories.SaleRepository;
 import com.danielvlima.vendas.repositories.SellerRepository;
@@ -28,4 +33,13 @@ public class SaleService {
 		return result.map(x -> new SaleDTO(x));
 	}
 	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+	}
 }
